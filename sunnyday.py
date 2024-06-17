1d1dcd5eea0ffeddf54daf2d7bd11aa2c71048ca
maksym solomyanov <maksym.solomyanov@gmail.com>
2024-06-17 18:36:48 +0200

    Initial commit

--- sunnyday.py
+++ sunnyday.py
@@ -1,10 +1,28 @@
 import requests
-from pprint import pprint
-import os
 
-API = os.getenv("OPENWEATHERAPI")
 
 class Weather:
+    """Creates a Weather object getting an apikey as input
+    and either a city name or lat and lon coordinates.
+
+    Package use example:
+
+    # Create a weather object using a city name:
+    # Get your apikey from https://openweathermap.org
+    # And wait a couple of hours for the apikey to be activated
+
+    # weather1 = Weather(apikey = "yourapikeyhere", city = "Rome"
+
+    # Using latitude and longitude coordinates
+    # weather2 = Weather(apikey = "yourapikeyhere", lat = 41.1, lon = -4.1)
+
+    # Get complete weather data for the next 12 hours:
+    # weather1.next_12h()
+
+    # Simplified data for the next 12 hours:
+    # weather1.next_12h_simplified()
+
+    """
 
     def __init__(self, apikey: str, city: str = None, lat: float = None, lon: float = None):
         if city:
@@ -29,7 +47,3 @@ class Weather:
             simple_data.append((dicty['dt_txt'], dicty['main']['temp'], dicty['weather'][0]['description']))
         return simple_data
 
-
-# weather = Weather(apikey=API, city="Rome")
-weather = Weather(apikey=API, city="Rome")
-pprint(weather.next_12h_simplified())
